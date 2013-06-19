1878353605c9830795db8a0f900ef295a4bbb246
Michael Engelhard <aijan.me+github@gmail.com>
2013-06-19 17:03:41 +0200

    refs #28 - added Scanner class and method for retrieving an image from a camera

---
+++ Go_Scanner/Scanner.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "opencv2/opencv.hpp"
+#include "GoSetup.h"
+
+#include <tuple>
+
+
+namespace Go_Scanner {
+
+class Scanner {
+public:
+    Scanner() {}
+
+private:
+    Scanner(const Scanner&);
+    Scanner& operator=(const Scanner&);
+        
+public:
+    /**
+    * @brief        currently only sets out_image to the read camera image
+    * @returns      true if a new image could be retrieved, false otherwise, may be changed to an enum or so
+    */
+    bool scanCamera(GoSetup& setup, int& board_size, cv::Mat& out_image);
+
+private:
+    /**
+    * @returns      true if a new image could be retrieved, false otherwise (camera disconnected)
+    */
+    bool readCameraFrame(cv::Mat& frame);
+
+private:
+    cv::VideoCapture _camera;
+    cv::Mat _last_frame;
+};
+
+}
---
+++ Go_Scanner/Scanner_class.cpp
@@ -0,0 +1,61 @@
+#include "Scanner.hpp"
+#include <iostream>
+
+
+namespace Go_Scanner {
+using namespace cv;
+using namespace std;
+
+bool Scanner::scanCamera(GoSetup& setup, int& board_size, cv::Mat& out_image) {
+    Mat frame;
+    if (!readCameraFrame(frame))
+        return false;
+
+    // do stuff with frame
+
+    out_image = frame;
+    return true;
+}
+
+bool Scanner::readCameraFrame(cv::Mat& frame) {
+    if (!_camera.isOpened()) {
+        // try opening camera 0
+        // when one camera is connected, it will always have id 0
+        bool opened = _camera.open(0);
+        if (!opened)
+            return false;
+
+        // just to have _last_frame initialized with the correct size (needed for compaision)
+        _camera.read(_last_frame);
+        // successive calls to read give the same data pointer
+        // setting the matrix to zero would then also set it to zero for the frame read below
+        _last_frame = _last_frame.clone();
+        _last_frame *= 0;
+    }
+
+    bool success = _camera.read(frame);
+
+    bool same_as_last_frame = false;
+    if (success) {
+        // success doesn't seem to indicate whether the camera has been disconnected or not,
+        // but read continuously returns the same image if it has been disconnected
+
+        // mask has 3 channels (rgb), but countNonZero only works on 1 channel matrices
+        // .reshape reinterprets the data as one channel
+        // it's a O(1) operation, as the new matrix points to the same data
+        Mat mask = _last_frame != frame;
+        same_as_last_frame = countNonZero(mask.reshape(1)) == 0;
+
+        _last_frame = frame.clone();
+    }
+
+    if (!success || same_as_last_frame) {
+        // camera has been disconnected
+        _camera.release();
+        return false;
+    }
+
+    return true;
+}
+
+}
