b1db5f24faabd8792074bf72655d25390770a535
Michael Engelhard <aijan.me+github@gmail.com>
2013-06-19 17:16:15 +0200

    refs #28 - added tests for scanner

---
+++ Go_Scanner_Test/scanner_tests.cpp
@@ -0,0 +1,35 @@
+#include "CppUnitTest.h"
+#include "Scanner.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace MyTest
+{
+    using Go_Scanner::Scanner;
+    using namespace cv;
+
+    TEST_CLASS(MyTests)
+    {
+    public:
+        TEST_METHOD(CameraReadingWorks)
+        {
+            // try plugging your camera in and out
+            // exit by pressing any key
+
+            Scanner scanner;
+            GoSetup setup;
+            int size;
+            Mat image;
+
+            while(true) {
+                bool success = scanner.scanCamera(setup, size, image);
+                if (success)
+                    imshow("after scanning: ", image);
+                else
+                    imshow("no image retrieved", Mat());
+
+                if(waitKey(30) >= 0)
+                    break;
+            }
+        }
+    };
+}
