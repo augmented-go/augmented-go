25534a63b87940724d9c13baa58fd9a3fc5499ea
Michael Engelhard <aijan.me+github@gmail.com>
2013-07-02 16:32:32 +0200

    Changed working directory of Go_Scanner_Test executable.

--- Go_Scanner_Test/main.cpp
+++ Go_Scanner_Test/main.cpp
@@ -1,20 +1,22 @@
 #include "Scanner.hpp"
+#include <QtCore/QDir>
+#include <QtCore/QCoreApplication>
 
 using Go_Scanner::Scanner;
 using namespace cv;
 
-int main() {
+int main(int argc, char** argv) {
+    QCoreApplication app(argc, argv);
+    QDir::setCurrent(QCoreApplication::applicationDirPath());
+
     // try plugging your camera in and out
     // exit by pressing any key
-
     Scanner scanner;
     GoSetup setup;
     int size;
     Mat image;
 
     while(true) {
-        // note: the standart debug picture won't work, as the working dir is not at the executable (with visual studio)
-        // camera works though
         bool success = scanner.scanCamera(setup, size, image);
         if (success)
             imshow("after scanning: ", image);
