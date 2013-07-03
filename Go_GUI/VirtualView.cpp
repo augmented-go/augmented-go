ac0336fb81fbb7c024734f3c1c9a1ca032a60dc1
Stefan Hufnagel <mail@stefan-hufnagel.de>
2013-07-03 21:50:57 +0200

    Refs #20 , added bigger images for board_9x9 and board_13x13, added some comments, added check if board_size is valid

--- Go_GUI/VirtualView.cpp
+++ Go_GUI/VirtualView.cpp
@@ -14,6 +14,10 @@ VirtualView::VirtualView(QWidget *parent){
 VirtualView::~VirtualView(){
 }
 
+/**
+ * @brief	Creates the virtual board of the go game and set the scene
+ * @param	QSize	size of the container	
+ */
 void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
 {
     if (game_board == nullptr)
@@ -21,16 +25,21 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
 
     QGraphicsScene* scene = new QGraphicsScene();
 
-    //directories of the images
-    QString texture_path = "res/textures/";
-    QString board_string = QString(texture_path + "go_board_19.png");
-    QString black_stone_string = QString(texture_path + "black_stone.png");
-    QString white_stone_string = QString(texture_path + "white_stone.png");
+	// loads the board size and checks if its a valid size
+	int board_size = game_board->Size();
+	if(board_size != 9 && board_size != 13 && board_size != 19)
+		QMessageBox::warning(this, "board size error", "invalid size of the board!");
 
-    //loads the images
-    QImage* board_image = new QImage(board_string);
-    QImage* black_stone_image = new QImage(black_stone_string);
-    QImage* white_stone_image = new QImage(white_stone_string);
+    // directories of the images
+    QString texture_path = "res/textures/";
+    QString board_directory = QString(texture_path + "go_board_"+QString::number(board_size)+".png");
+    QString black_stone_directory = QString(texture_path + "black_stone.png");
+    QString white_stone_directory = QString(texture_path + "white_stone.png");
+
+    // loads the images and checks if the image could loaded
+    QImage* board_image = new QImage(board_directory);
+    QImage* black_stone_image = new QImage(black_stone_directory);
+    QImage* white_stone_image = new QImage(white_stone_directory);
 
     if (board_image->isNull())
         QMessageBox::warning(this, "file loading error", "could not load board image!");
@@ -39,17 +48,14 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
     if (white_stone_image->isNull())
         QMessageBox::warning(this, "file loading error", "could not laod white stone image!");
 
-    //TODO: load board size from backend
-    int board_size = game_board->Size();
-
-    //scale_x and scale_y are the scaling factors of the virtual board
+    // scale_x and scale_y are the scaling factors of the virtual board
     float scale_x = size.width() / float(board_image->width());
     float scale_y = size.height() / float(board_image->height());
 
     float cell_width = float(board_image->width()) / (board_size+1);
     float cell_height = float(board_image->height()) / (board_size+1);
 
-    //scale the images to the right size
+    // scale the images to the right size
     QPixmap board_image_scaled = QPixmap::fromImage(*board_image);
     board_image_scaled = board_image_scaled.scaled(size.width(),size.height());
 
@@ -66,53 +72,10 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
     if (white_stone_image_scaled.isNull())
         QMessageBox::warning(this, "image scale error", "could not scale white stone!");
 
-    //add the board image to the scene
+    // add the board image to the scene
     scene->addItem(new QGraphicsPixmapItem(board_image_scaled));
 
-    ////this is a 19x19 test array to display a board with stones. (0 = noone; 1 = black; 2 = white)
-    //int test [19][19] = {   {1,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1}, 
-    //                        {1,1,2,0,0,0,0,0,0,2,0,0,0,0,0,0,0,1,1},
-    //                        {0,0,2,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,1},
-    //                        {0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,1,1,2,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,1,2,1,1,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,1,1,1,1,2,2,1,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1},
-    //                        {0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,1},
-    //                        {1,0,0,0,0,0,0,0,0,2,2,2,2,1,1,1,0,0,1} }; 
-    //
-    ////add the stones to the scene at the right position like in the test array above
-    //for(int row = 0; row<19; row++)
-    //{
-    //    for(int col = 0; col<19; col++)
-    //    {
-    //        if(test[row][col] == 1)
-    //        {
-    //            QGraphicsPixmapItem* black_stone_item = new QGraphicsPixmapItem(black_stone_image_scaled);
-    //            black_stone_item->setPos(cell_width * scale_x * col, cell_height * scale_y * row);
-    //            black_stone_item->setOffset(cell_width * scale_x - black_stone_image_scaled.width()/2, cell_height * scale_y - black_stone_image_scaled.height()/2);
-    //            scene->addItem(black_stone_item);
-    //        }
-    //        if(test[row][col] == 2)
-    //        {
-    //            QGraphicsPixmapItem* white_stone_item = new QGraphicsPixmapItem(white_stone_image_scaled);
-    //            white_stone_item->setPos(cell_width * scale_x * col, cell_height * scale_y * row);
-    //            white_stone_item->setOffset(cell_width * scale_x - white_stone_image_scaled.width()/2, cell_height * scale_y - white_stone_image_scaled.height()/2);
-    //            scene->addItem(white_stone_item);
-    //        }
-    //    }
-    //}
-
-    // get all stone positions for each color
+    // get all stone positions for each color and add them on the right position to the scene
     auto black_stones = game_board->All(SG_BLACK);
     for (auto iter = SgSetIterator(black_stones); iter; ++iter) {
         auto point = *iter;
@@ -138,8 +101,6 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
         black_stone_item->setOffset(cell_width * scale_x - white_stone_image_scaled.width()/2, cell_height * scale_y - black_stone_image_scaled.height()/2);
         scene->addItem(black_stone_item);
     }
-
-
 
     this->setScene(scene);
 }
