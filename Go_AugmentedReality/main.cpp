532a0984d27d48a430a78e98d376575e556e282e
Jens Schmer <der.liggers@gmail.com>
2013-07-20 19:09:22 +0200

    formatted signal/slot connections

--- Go_AugmentedReality/main.cpp
+++ Go_AugmentedReality/main.cpp
@@ -21,23 +21,23 @@ int main(int argc, char** argv) {
         GUI gui;
 
         // connect signal from backend to gui
-        QObject::connect(&backend, &BackendThread::newImage, &gui, &GUI::slot_newImage, Qt::QueuedConnection);
-        QObject::connect(&backend, &BackendThread::gameDataChanged, &gui, &GUI::slot_newGameData, Qt::QueuedConnection);
+        QObject::connect(&backend, &BackendThread::newImage,           &gui, &GUI::slot_newImage,                Qt::QueuedConnection);
+        QObject::connect(&backend, &BackendThread::gameDataChanged,    &gui, &GUI::slot_newGameData,             Qt::QueuedConnection);
         QObject::connect(&backend, &BackendThread::finishedGameResult, &gui, &GUI::slot_showFinishedGameResults, Qt::QueuedConnection);
 
         // connect signal from gui to backend
-        QObject::connect(&gui, &GUI::stop_backend_thread, &backend, &BackendThread::stop, Qt::QueuedConnection);
-        QObject::connect(&gui, &GUI::signal_saveGame, &backend, &BackendThread::saveSgf, Qt::QueuedConnection);
-        QObject::connect(&gui, &GUI::signal_pass, &backend, &BackendThread::pass, Qt::QueuedConnection);
-        QObject::connect(&gui, &GUI::signal_resign, &backend, &BackendThread::resign, Qt::QueuedConnection);
+        QObject::connect(&gui, &GUI::stop_backend_thread, &backend, &BackendThread::stop,    Qt::QueuedConnection);
+        QObject::connect(&gui, &GUI::signal_saveGame,     &backend, &BackendThread::saveSgf, Qt::QueuedConnection);
+        QObject::connect(&gui, &GUI::signal_pass,         &backend, &BackendThread::pass,    Qt::QueuedConnection);
+        QObject::connect(&gui, &GUI::signal_resign,       &backend, &BackendThread::resign,  Qt::QueuedConnection);
 
         backend.start(); // start backend thread
 
         gui.show();
         qt_app.exec();   // start gui thread (and it's event loop)
     
-        backend.quit();  // failsafe: explicitly tell the backend thread to stop (if the gui hasn't done this)
-        backend.wait();  // gui was exited, wait for the backend thread (gui should have sent a signal to the backend thread to quit)
+        backend.quit();  // failsafe: explicitly tell the backend thread to stop (if the gui hasn't done this already for whatever reason)
+        backend.wait();  // gui was exited, wait for the backend thread
     } 
 
     // "clean up" fuego
