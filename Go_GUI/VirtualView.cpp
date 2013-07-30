9f7503f797724917f957401ce8454a6356087a3c
Christian Petry <petry2christian@gmail.com>
2013-07-30 10:38:12 +0200

    ref # 69 Creates images on stack, instead of heap. QGraphicsItems are removed and deleted once GraphicsScene is cleared!

--- Go_GUI/VirtualView.cpp
+++ Go_GUI/VirtualView.cpp
@@ -10,8 +10,6 @@
 
 VirtualView::VirtualView(QWidget *parent){
     this->setParent(parent);
-    
-    scene = new QGraphicsScene();
 
     // directories of the images
     QString texture_path = "res/textures/";
@@ -22,11 +20,11 @@ VirtualView::VirtualView(QWidget *parent){
     QString white_stone_directory = QString(texture_path + "white_stone.png");
 
     // loads the images and checks if the image could loaded
-    board_image_size9 = new QImage(board_directory_size9);
-    board_image_size13 = new QImage(board_directory_size13);
-    board_image_size19 = new QImage(board_directory_size19);
-    black_stone_image = new QImage(black_stone_directory);
-    white_stone_image = new QImage(white_stone_directory);
+    board_image_size9 = QImage(board_directory_size9);
+    board_image_size13 = QImage(board_directory_size13);
+    board_image_size19 = QImage(board_directory_size19);
+    black_stone_image = QImage(black_stone_directory);
+    white_stone_image = QImage(white_stone_directory);
 
 }
 VirtualView::~VirtualView(){
@@ -41,12 +39,12 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
     if (game_board == nullptr)
         return;
 
-    scene->clear();
+    scene.clear();
     
     // loads the board size and checks if its a valid size
     int board_size = game_board->Size();
 
-    QImage* board_image;
+    QImage board_image;
 
     switch(board_size){
     case 9:
@@ -62,29 +60,29 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
         QMessageBox::warning(this, "board size error", "invalid size of the board!");
     }
 
-    if (board_image->isNull())
+    if (board_image.isNull())
         QMessageBox::warning(this, "file loading error", "could not load board image!");
-    if (black_stone_image->isNull())
+    if (black_stone_image.isNull())
         QMessageBox::warning(this, "file loading error", "could not load black stone image!");
-    if (white_stone_image->isNull())
+    if (white_stone_image.isNull())
         QMessageBox::warning(this, "file loading error", "could not laod white stone image!");
 
     // scale_x and scale_y are the scaling factors of the virtual board
-    float scale_x = size.width() / float(board_image->width());
-    float scale_y = size.height() / float(board_image->height());
+    float scale_x = size.width() / float(board_image.width());
+    float scale_y = size.height() / float(board_image.height());
 
-    float cell_width = float(board_image->width()) / (board_size+1);
-    float cell_height = float(board_image->height()) / (board_size+1);
+    float cell_width = float(board_image.width()) / (board_size+1);
+    float cell_height = float(board_image.height()) / (board_size+1);
 
     // scale the images to the right size
-    QPixmap board_image_scaled = QPixmap::fromImage(*board_image);
+    QPixmap board_image_scaled = QPixmap::fromImage(board_image);
     board_image_scaled = board_image_scaled.scaled(size.width(),size.height());
 
-    QPixmap black_stone_image_scaled = QPixmap::fromImage(*black_stone_image);
-    black_stone_image_scaled = black_stone_image_scaled.scaled(black_stone_image->width()*scale_x, black_stone_image->height()*scale_y);
+    QPixmap black_stone_image_scaled = QPixmap::fromImage(black_stone_image);
+    black_stone_image_scaled = black_stone_image_scaled.scaled(black_stone_image.width()*scale_x, black_stone_image.height()*scale_y);
 
-    QPixmap white_stone_image_scaled = QPixmap::fromImage(*white_stone_image);
-    white_stone_image_scaled = white_stone_image_scaled.scaled(white_stone_image->width()*scale_x, white_stone_image->height()*scale_y);
+    QPixmap white_stone_image_scaled = QPixmap::fromImage(white_stone_image);
+    white_stone_image_scaled = white_stone_image_scaled.scaled(white_stone_image.width()*scale_x, white_stone_image.height()*scale_y);
 
     if (board_image_scaled.isNull())
         QMessageBox::warning(this, "image scale error", "could not scale board!");
@@ -94,7 +92,7 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
         QMessageBox::warning(this, "image scale error", "could not scale white stone!");
 
     // add the board image to the scene
-    scene->addItem(new QGraphicsPixmapItem(board_image_scaled));
+    scene.addItem(new QGraphicsPixmapItem(board_image_scaled));
 
     // get all stone positions for each color and add them on the right position to the scene
     auto black_stones = game_board->All(SG_BLACK);
@@ -111,7 +109,7 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
         QGraphicsPixmapItem* black_stone_item = new QGraphicsPixmapItem(black_stone_image_scaled);
         black_stone_item->setPos(cell_width * scale_x * col, cell_height * scale_y * row);
         black_stone_item->setOffset(cell_width * scale_x - black_stone_image_scaled.width()/2, cell_height * scale_y - black_stone_image_scaled.height()/2);
-        scene->addItem(black_stone_item);
+        scene.addItem(black_stone_item);
     }
 
     auto white_stones = game_board->All(SG_WHITE);
@@ -128,8 +126,8 @@ void VirtualView::createAndSetScene(QSize size, const GoBoard * game_board)
         QGraphicsPixmapItem* black_stone_item = new QGraphicsPixmapItem(white_stone_image_scaled);
         black_stone_item->setPos(cell_width * scale_x * col, cell_height * scale_y * row);
         black_stone_item->setOffset(cell_width * scale_x - white_stone_image_scaled.width()/2, cell_height * scale_y - black_stone_image_scaled.height()/2);
-        scene->addItem(black_stone_item);
+        scene.addItem(black_stone_item);
     }
-    this->setScene(scene);
+    this->setScene(&scene);
 }
 
