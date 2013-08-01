824fce460baff0cdcbf40ccaeaf159c0d010a7a6
Jens Schmer <der.liggers@gmail.com>
2013-08-01 14:27:26 +0200

    [closes #78] renamed Go_AugmentedReality to Go_Controller and Go_Backend to Go_Game to make clear what the intention of these modules are

--- cmake/add_backend_to_target.cmake
+++
@@ -1,6 +0,0 @@
-# links the GoBackend library to the specified target
-function(add_backend_to_target target)
-    include_directories (${CMAKE_SOURCE_DIR}/Go_Backend)
-    link_directories (${CMAKE_BINARY_DIR}/Go_Backend)
-    target_link_libraries (${target} Go_Backend)
-endfunction()
