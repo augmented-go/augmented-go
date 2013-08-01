848c8cd77aae4ca65e36a926dd67ba7084b08f24
Jens Schmer <der.liggers@gmail.com>
2013-08-01 14:46:47 +0200

    [refs #78] Reverted back to the Go_Backend name

--- cmake/add_game_to_target.cmake
+++
@@ -1,6 +0,0 @@
-# links the GoBackend library to the specified target
-function(add_game_to_target target)
-    include_directories (${CMAKE_SOURCE_DIR}/Go_Game)
-    link_directories (${CMAKE_BINARY_DIR}/Go_Game)
-    target_link_libraries (${target} Go_Game)
-endfunction()
