cc6cfb2df42a9bd5189e168644541a7c63e70d89
Jens Schmer <der.liggers@gmail.com>
2013-08-02 17:57:53 +0200

    [refs #93] Fixed some problems when destroying objects that are moved to another thread

--- Go_Controller/main.cpp
+++ Go_Controller/main.cpp
@@ -17,39 +17,42 @@ int main(int argc, char** argv) {
         using Go_AR::BackendWorker;
         using Go_GUI::GUI;
 
-        QThread worker_thread;
-        BackendWorker worker;
-        worker.moveToThread(&worker_thread);
-
         GUI gui;
+
+        // The worker can't be on the stack because of Qts signals when object are destroyed,
+        // these would trigger assertion errors that say that you can't send events to a different thread
+        auto worker = new BackendWorker();
+
+        QThread worker_thread;
+        QObject::connect( &worker_thread, SIGNAL(finished()), worker, SLOT(deleteLater()) ); // clean up the worker when the thread is stopped
+
+        // move the worker into the thread
+        worker->moveToThread(&worker_thread);
 
         qRegisterMetaType<GoRules>("GoRules");
 
         // connect signal from worker to gui
-        QObject::connect(&worker, &BackendWorker::newImage,           &gui, &GUI::slot_newImage);
-        QObject::connect(&worker, &BackendWorker::gameDataChanged,    &gui, &GUI::slot_newGameData);
-        QObject::connect(&worker, &BackendWorker::finishedGameResult, &gui, &GUI::slot_showFinishedGameResults);
-
-        // connect quit signal from gui to the thread
-        QObject::connect(&gui, &GUI::stop_backend_thread,                &worker_thread, &QThread::quit);
+        QObject::connect(worker, &BackendWorker::newImage,           &gui, &GUI::slot_newImage);
+        QObject::connect(worker, &BackendWorker::gameDataChanged,    &gui, &GUI::slot_newGameData);
+        QObject::connect(worker, &BackendWorker::finishedGameResult, &gui, &GUI::slot_showFinishedGameResults);
 
         // connect signal from gui to worker
-        QObject::connect(&gui, &GUI::signal_saveGame,                    &worker, &BackendWorker::saveSgf);
-        QObject::connect(&gui, &GUI::signal_pass,                        &worker, &BackendWorker::pass);
-        QObject::connect(&gui, &GUI::signal_resign,                      &worker, &BackendWorker::resign);
-        QObject::connect(&gui, &GUI::signal_newGame,                     &worker, &BackendWorker::resetGame);
-        QObject::connect(&gui, &GUI::signal_boardDetectionAutomatically, &worker, &BackendWorker::selectBoardAutomatically);
-        QObject::connect(&gui, &GUI::signal_boardDetectionManually,      &worker, &BackendWorker::selectBoardManually);
-        QObject::connect(&gui, &GUI::signal_toggleAppMode,               &worker, &BackendWorker::toggleAppMode);
-        QObject::connect(&gui, &GUI::signal_playMove,                    &worker, &BackendWorker::playMove);
+        QObject::connect(&gui, &GUI::signal_saveGame,                    worker, &BackendWorker::saveSgf);
+        QObject::connect(&gui, &GUI::signal_pass,                        worker, &BackendWorker::pass);
+        QObject::connect(&gui, &GUI::signal_resign,                      worker, &BackendWorker::resign);
+        QObject::connect(&gui, &GUI::signal_newGame,                     worker, &BackendWorker::resetGame);
+        QObject::connect(&gui, &GUI::signal_boardDetectionAutomatically, worker, &BackendWorker::selectBoardAutomatically);
+        QObject::connect(&gui, &GUI::signal_boardDetectionManually,      worker, &BackendWorker::selectBoardManually);
+        QObject::connect(&gui, &GUI::signal_toggleAppMode,               worker, &BackendWorker::toggleAppMode);
+        QObject::connect(&gui, &GUI::signal_playMove,                    worker, &BackendWorker::playMove);
 
         worker_thread.start(); // start worker thread
 
         gui.show();
         qt_app.exec();   // start gui thread (and it's event loop)
-    
-        worker_thread.quit();  // failsafe: explicitly tell the worker thread to stop (if the gui hasn't done this already for whatever reason)
-        worker_thread.wait();  // gui was exited, wait for the worker thread
+
+        worker_thread.quit();
+        worker_thread.wait();
     } 
 
     // "clean up" fuego
