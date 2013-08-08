dce7bfc6774ad6ed91e3190d0060537c4837f234
Michael Engelhard <aijan.me+github@gmail.com>
2013-08-08 18:12:21 +0200

    Removed the CameraReadingWorks "unittest". It wasn't really a unittest and you can now easily check if the camera works by using the gui anyway.

--- Go_Scanner_Unittest/scanner_tests.cpp
+++ Go_Scanner_Unittest/scanner_tests.cpp
@@ -10,24 +10,6 @@ namespace MyTest
     TEST_CLASS(MyTests)
     {
     public:
-        TEST_METHOD(CameraReadingWorks)
-        {
-            // try plugging your camera in and out
-            // exit by pressing any key
 
-            Scanner scanner;
-            GoSetup setup;
-            int size;
-            Mat image;
-
-            while(true) {
-                auto result = scanner.scanCamera(setup, size, image);
-                if (result == Go_Scanner::ScanResult::Image_Only || Go_Scanner::ScanResult::Success)
-                    imshow("after scanning: ", image);
-
-                if(waitKey(30) >= 0)
-                    break;
-            }
-        }
     };
 }
