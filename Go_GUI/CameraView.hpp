1496fc7899b0a1fc5b4468b2262787697d7d5f64
Christian Petry <petry2christian@gmail.com>
2013-09-25 15:10:56 +0200

    ref #163 added files

---
+++ Go_GUI/CameraView.hpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2013 augmented-go team
+// See the file LICENSE for full license and copying terms.
+#pragma once
+#include <QWidget>
+#include <QLabel>
+#include <QPixmap>
+#include <QMessageBox>
+
+namespace Go_GUI {
+
+/**
+ * @class   CameraView
+ * @brief   The CameraView is part of Augmented-Go Gui.
+ *          The view shows the camera picture (if a camera is connected) or the augmented logo.
+ *			If an error occurs an error message is shown on top of the camera picture.
+ *          If the game is in virtual mode, the user can place stones by clicking on the board.
+ */
+class CameraView : public QLabel
+{
+    Q_OBJECT
+public:
+
+    /**
+     * @brief	Constructor
+     *          Sets its parent widget,
+     *          Sets the alignment of its content to center
+     *          Sets up the Augmented Logo as its standard picture it shows and resizes it
+     */
+    CameraView(QWidget *parent = 0) {
+        this->setParent(parent);
+        this->setAlignment(Qt::AlignCenter);
+
+        // Setting up augmented logo as standard picture
+        QString augmented_logo_path = "res/textures/augmented_logo.png";
+        if(QImage(augmented_logo_path).isNull())
+            QMessageBox::critical(this, "GUI element not found", "Warning image of AugmentedView not found!\n searched relative to exe in" + augmented_logo_path);
+        else{
+            setImage(QImage(augmented_logo_path));
+            rescaleImage(parentWidget()->size());
+        }
+    }
+    ~CameraView(){};
+
+    /**
+     * @brief		Sets the new image that shall be displayed.
+     *				If the new image is empty, show the same picture as before.
+     *				Default picture is "no_camera_picture.png".
+     * @parameter	QImage	image that shall be displayed. 
+     */
+    void setImage(QImage image){
+        if(!image.isNull())		// if image is empty take old picture!
+            picture = QPixmap::fromImage(image);
+
+        this->setPixmap(picture);
+        this->show();
+    }
+
+    /**
+     * @brief		scales the pixmap to the width of size
+     * @parameter	QSize
+     */
+    void rescaleImage(QSize size){
+        this->resize(size);
+        if (!picture.isNull())
+            setPixmap(picture.scaled(size, Qt::KeepAspectRatio));
+    }
+
+private:
+    QPixmap picture;
+};
+
+}
